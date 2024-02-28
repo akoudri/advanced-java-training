edacd2a44fbff5f251b4951d93ded1a99c4336a1
akoudri <akoudri@cenotelie.fr>
2024-02-28 17:21:44 +0100

    Cleans lambda exercise

--- src/main/java/fr/cenotelie/training/streams/Lambdas.java
+++ src/main/java/fr/cenotelie/training/streams/Lambdas.java
@@ -1,7 +1,5 @@
 package fr.cenotelie.training.streams;
 
-import java.util.function.Function;
-
 public class Lambdas {
     public static void main(String[] args) {
         //TODO: compose x*x and x+5
