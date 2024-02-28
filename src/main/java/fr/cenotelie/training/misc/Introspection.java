fc7ec955ce92601d45318ceeec9a04ad870e88c0
akoudri <akoudri@cenotelie.fr>
2024-02-28 10:40:11 +0100

    Cleans introspection exercise

--- src/main/java/fr/cenotelie/training/misc/Introspection.java
+++ src/main/java/fr/cenotelie/training/misc/Introspection.java
@@ -1,9 +1,5 @@
 package fr.cenotelie.training.misc;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
 public class Introspection {
     public static void main(String[] args) {
         String className = "java.util.ArrayList"; // Fully qualified class name
