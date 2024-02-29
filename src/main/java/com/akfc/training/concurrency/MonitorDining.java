def12810b84e3216384c06134660a9238e3c4ee7
akoudri <akoudri@cenotelie.fr>
2024-02-29 11:05:41 +0100

    Fixes Monitor Dining exercise

--- src/main/java/com/akfc/training/concurrency/MonitorDining.java
+++ src/main/java/com/akfc/training/concurrency/MonitorDining.java
@@ -6,23 +6,24 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class MonitorDining {
 
+    private static final int TOTAL_SWEETS = 20;
+    private static final Lock lock = new ReentrantLock();
+    private static final Condition canEat = lock.newCondition();
+    private static int sweetsLeft = TOTAL_SWEETS;
+
     public static void main(String[] args) throws InterruptedException {
         Thread[] philosophers = new Thread[5];
-        for (int i = 0; i < 5; i++) {
-            philosophers[i] = new Philosopher(i);
+        for (int i = 0; i < philosophers.length; i++) {
+            philosophers[i] = new Thread(new Philosopher(i), "Philosopher " + i);
             philosophers[i].start();
         }
-        for (int i = 0; i < 5; i++) {
-            philosophers[i].join();
+        for (Thread philosopher : philosophers) {
+            philosopher.join();
         }
     }
 
-    static class Philosopher extends Thread {
-        private int id;
-        static Lock fork = new ReentrantLock();
-        static int sweets = 20;
-        private static Condition sweetTaken = fork.newCondition();
-        private static int totalSweetsConsumed = 0;
+    static class Philosopher implements Runnable {
+        private final int id;
 
         public Philosopher(int id) {
             this.id = id;
@@ -30,22 +31,28 @@ public class MonitorDining {
 
         @Override
         public void run() {
-            while (totalSweetsConsumed < 100) {
-                fork.lock();
+            while (sweetsLeft >= 0) {
+                lock.lock();
                 try {
-                    while (sweets == 0) {
-                        try {
-                            sweetTaken.await();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                    while (sweetsLeft == 0) {
+                        canEat.await(); // Philosophers wait here if no sweets are left
                     }
-                    sweets--;
-                    totalSweetsConsumed++;
-                    System.out.format("%s has eaten one sweet\n", this.getName());
+                    if (sweetsLeft <= 0) {
+                        break;
+                    }
+                    // Exit condition: Check after being awakened in case all sweets are done.
+                    sweetsLeft--; // A sweet is consumed
+                    System.out.println(Thread.currentThread().getName() + " ate a sweet. Sweets left: " + sweetsLeft);
+
+                    // After consuming a sweet, signal others that they might be able to eat now.
+                    canEat.signalAll();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    System.out.println(Thread.currentThread().getName() + " was interrupted.");
+                    // Exit the loop and end the thread upon interruption.
+                    return;
                 } finally {
-                    sweetTaken.signal();  // Move the signal inside the finally block
-                    fork.unlock();
+                    lock.unlock();
                 }
             }
         }
