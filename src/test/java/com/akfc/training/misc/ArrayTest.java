bf1206776bae2e3cb94c09064548833d236c1ca1
akoudri <akoudri@cenotelie.fr>
2024-03-04 19:49:02 +0100

    Improves configuration

---
+++ src/test/java/com/akfc/training/misc/ArrayTest.java
@@ -0,0 +1,21 @@
+package com.akfc.training.misc;
+
+import org.junit.jupiter.api.Test;
+
+class ArrayTest {
+
+    @Test
+    void bubblesSort() {
+        //TODO: Implement this test
+    }
+
+    @Test
+    void insertionSort() {
+        //TODO: Implement this test
+    }
+
+    @Test
+    void quickSort() {
+        //TODO: Implement this test
+    }
+}
